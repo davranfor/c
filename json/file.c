a52ca6c69c08b0adcfaa4a21ea3cf753618e116c
David Ranieri <50286622+davranfor@users.noreply.github.com>
2019-10-07 17:46:49 +0200

    Add files via upload
    Added files for file management

---
+++ json/file.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "file.h"
+
+long file_size(FILE *file)
+{
+    if (fseek(file, 0L, SEEK_END) == -1)
+    {
+        perror("fseek");
+        return -1;
+    }
+
+    long size = ftell(file);
+
+    if (size == -1)
+    {
+        perror("ftell");
+        return -1;
+    }
+    if (fseek(file, 0L, SEEK_SET) == -1)
+    {
+        perror("fseek");
+        return -1;
+    }
+    return size;
+}
+
+char *file_gets(FILE *file, size_t size)
+{
+    char *str = malloc(size + 1);
+
+    if (str == NULL)
+    {
+        perror("malloc");
+        return NULL;
+    }
+    if (fread(str, 1, size, file) != size)
+    {
+        free(str);
+        perror("fread");
+        return NULL;
+    }
+    str[size] = '\0';
+    return str;
+}
+
+char *file_read(const char *path)
+{
+    FILE *file = fopen(path, "r");
+
+    if (file == NULL)
+    {
+        perror("fopen");
+        return NULL;
+    }
+
+    long size = file_size(file);
+
+    if (size == -1)
+    {
+        fclose(file);
+        return NULL;
+    }    
+
+    char *str = file_gets(file, (size_t)size);
+
+    if (str == NULL)
+    {
+        fclose(file);
+        return NULL;
+    }
+    fclose(file);
+    return str;
+}
+
--- json/main.c
+++ json/main.c
@@ -1,51 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include "file.h"
 #include "json.h"
-
-static char *file_read(const char *path)
-{
-    FILE *file = fopen(path, "rb");
-
-    if (file == NULL)
-    {
-        perror("fopen");
-        return NULL;
-    }
-    if (fseek(file, 0, SEEK_END) == -1)
-    {
-        perror("fseek");
-        return NULL;
-    }
-
-    long size = ftell(file);
-
-    if (size == -1)
-    {
-        perror("ftell");
-        return NULL;
-    }
-    if (fseek(file, 0, SEEK_SET) == -1)
-    {
-        perror("fseek");
-        return NULL;
-    }
-
-    char *str = malloc((size_t)size + 1);
-
-    if (str == NULL)
-    {
-        return NULL;
-    }
-    if (fread(str, 1, (size_t)size, file) != (size_t)size)
-    {
-        perror("fread");
-        return NULL;
-    }
-    str[size] = '\0';
-    fclose(file);
-    return str;
-}
 
 int main(void)
 {
