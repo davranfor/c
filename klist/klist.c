e9cb84912c50a64ea88c8f794cfd7f2523f5cf4c
David Ranieri <50286622+davranfor@users.noreply.github.com>
2020-12-18 21:42:26 +0100

    Add files via upload

--- klist/klist.c
+++ klist/klist.c
@@ -8,18 +8,16 @@
 #include <stdalign.h>
 #include "klist.h"
 
-typedef struct knode knode;
-
-struct knode
+struct node
 {
-    knode *next;
-    knode *prev;
+    struct node *next;
+    struct node *prev;
 };
 
 struct klist
 {
-    knode *head;
-    knode *tail;
+    struct node *head;
+    struct node *tail;
     size_t szof;
     size_t size;
 };
@@ -30,28 +28,28 @@ klist *klist_create(size_t size)
 
     if (list != NULL)
     {
-        size_t align = alignof(knode);
+        size_t align = alignof(struct node);
 
-        // Round size up to nearest multiple of alignof(knode)
+        // Round size up to nearest multiple of alignof(struct node)
         list->szof = (size + (align - 1)) / align * align;
     }
     return list;
 }
 
-#define klist_const_node(list, data) ((const knode *)((const char *)data + list->szof))
-#define klist_node(list, data) ((knode *)((char *)data + list->szof))
+#define klist_const_node(list, data) ((const struct node *)((const char *)data + list->szof))
+#define klist_node(list, data) ((struct node *)((char *)data + list->szof))
 #define klist_data(list, node) ((char *)node - list->szof)
 
 void *klist_push_head(klist *list)
 {
-    void *data = calloc(1, list->szof + sizeof(knode));
+    void *data = calloc(1, list->szof + sizeof(struct node));
 
     if (data == NULL)
     {
         return NULL;
     }
 
-    knode *node = klist_node(list, data);
+    struct node *node = klist_node(list, data);
 
     if (list->head != NULL)
     {
@@ -69,14 +67,14 @@ void *klist_push_head(klist *list)
 
 void *klist_push_tail(klist *list)
 {
-    void *data = calloc(1, list->szof + sizeof(knode));
+    void *data = calloc(1, list->szof + sizeof(struct node));
 
     if (data == NULL)
     {
         return NULL;
     }
 
-    knode *node = klist_node(list, data);
+    struct node *node = klist_node(list, data);
 
     if (list->tail != NULL)
     {
@@ -94,17 +92,20 @@ void *klist_push_tail(klist *list)
 
 void *klist_pop_head(klist *list)
 {
-    if (list->head == NULL)
+    struct node *node = list->head;
+
+    if (node == NULL)
     {
         return NULL;
     }
 
-    void *data = klist_data(list, list->head);
+    void *data = klist_data(list, node);
 
-    list->head = list->head->next;
-    if (list->head != NULL)
+    list->head = node->next;
+    if (node->next != NULL)
     {
-        list->head->prev = NULL;
+        node->next->prev = NULL;
+        node->next = NULL;
     }
     else
     {
@@ -116,17 +117,20 @@ void *klist_pop_head(klist *list)
 
 void *klist_pop_tail(klist *list)
 {
-    if (list->tail == NULL)
+    struct node *node = list->tail;
+
+    if (node == NULL)
     {
         return NULL;
     }
 
-    void *data = klist_data(list, list->tail);
+    void *data = klist_data(list, node);
 
-    list->tail = list->tail->prev;
-    if (list->tail != NULL)
+    list->tail = node->prev;
+    if (node->prev != NULL)
     {
-        list->tail->next = NULL;
+        node->prev->next = NULL;
+        node->prev = NULL;
     }
     else
     {
@@ -136,9 +140,9 @@ void *klist_pop_tail(klist *list)
     return data;
 }
 
-static knode *klist_get(const klist *list, size_t index)
+static struct node *klist_get(const klist *list, size_t index)
 {
-    knode *node;
+    struct node *node;
 
     if (list->size - index > index)
     {
@@ -170,15 +174,15 @@ void *klist_insert(klist *list, size_t index)
         return klist_push_tail(list);
     }
 
-    void *data = calloc(1, list->szof + sizeof(knode));
+    void *data = calloc(1, list->szof + sizeof(struct node));
 
     if (data == NULL)
     {
         return NULL;
     }
 
-    knode *node = klist_node(list, data); 
-    knode *curr = klist_get(list, index);
+    struct node *node = klist_node(list, data); 
+    struct node *curr = klist_get(list, index);
 
     curr->prev->next = node;
     node->prev = curr->prev;
@@ -203,7 +207,7 @@ void *klist_delete(klist *list, size_t index)
         return klist_pop_tail(list);
     }
 
-    knode *node = klist_get(list, index);
+    struct node *node = klist_get(list, index);
     void *data = klist_data(list, node);
 
     node->prev->next = node->next;
@@ -234,7 +238,7 @@ void *klist_prev(const klist *list, const void *data)
 {
     if (data != NULL)
     {
-        const knode *node = klist_const_node(list, data);
+        const struct node *node = klist_const_node(list, data);
 
         if (node->prev != NULL)
         {
@@ -248,7 +252,7 @@ void *klist_next(const klist *list, const void *data)
 {
     if (data != NULL)
     {
-        const knode *node = klist_const_node(list, data);
+        const struct node *node = klist_const_node(list, data);
 
         if (node->next != NULL)
         {
@@ -272,10 +276,10 @@ size_t klist_size(const klist *list)
     return list->size;
 }
 
-static knode *split(knode *head)
+static struct node *split(struct node *head)
 {
-    knode *fast = head;
-    knode *slow = head;
+    struct node *fast = head;
+    struct node *slow = head;
 
     while ((fast->next != NULL) && (fast->next->next != NULL))
     {
@@ -283,14 +287,14 @@ static knode *split(knode *head)
         slow = slow->next;
     }
 
-    knode *temp = slow->next;
+    struct node *temp = slow->next;
 
     slow->next = NULL;
     return temp;
 }
 
 /* Recursive version causing stack overflow on large datatsets
-static knode *merge(klist *list, knode *head, knode *tail, int (*comp)(const void *, const void *))
+static struct node *merge(klist *list, struct node *head, struct node *tail, int (*comp)(const void *, const void *))
 {
     if (head == NULL)
     {
@@ -314,7 +318,7 @@ static knode *merge(klist *list, knode *head, knode *tail, int (*comp)(const voi
 */
 
 /* Iterative version */
-static knode *merge(klist *list, knode *head, knode *tail, int (*comp)(const void *, const void *))
+static struct node *merge(klist *list, struct node *head, struct node *tail, int (*comp)(const void *, const void *))
 {
     if (head == NULL)
     {
@@ -325,11 +329,11 @@ static knode *merge(klist *list, knode *head, knode *tail, int (*comp)(const voi
         return head;
     }
 
-    knode *curr = head;
+    struct node *curr = head;
 
     while (tail != NULL)
     {
-        knode *prev = tail;
+        struct node *prev = tail;
 
         tail = tail->next;
         prev->next = NULL;
@@ -355,7 +359,7 @@ static knode *merge(klist *list, knode *head, knode *tail, int (*comp)(const voi
             }
             else
             {
-                knode *temp;
+                struct node *temp;
 
                 temp = curr->next;
                 curr->next = prev;
@@ -367,14 +371,14 @@ static knode *merge(klist *list, knode *head, knode *tail, int (*comp)(const voi
     return head;
 }
 
-static knode *sort(klist *list, knode *head, int (*comp)(const void *, const void *))
+static struct node *sort(klist *list, struct node *head, int (*comp)(const void *, const void *))
 {
     if ((head == NULL) || (head->next == NULL))
     {
         return head;
     }
 
-    knode *tail = split(head);
+    struct node *tail = split(head);
 
     head = sort(list, head, comp);
     tail = sort(list, tail, comp);
@@ -393,7 +397,7 @@ void klist_sort(klist *list, int (*comp)(const void *, const void *))
         list->head = sort(list, list->head, comp);
         list->head->prev = NULL;
 
-        knode *node = list->head;
+        struct node *node = list->head;
 
         while (node->next != NULL)
         {
@@ -406,7 +410,7 @@ void klist_sort(klist *list, int (*comp)(const void *, const void *))
 
 void *klist_search(const klist *list, const void *data, int (*comp)(const void *, const void *))
 {
-    knode *iter;
+    struct node *iter;
 
     for (iter = list->head; iter != NULL; iter = iter->next)
     {
@@ -422,14 +426,14 @@ void *klist_search(const klist *list, const void *data, int (*comp)(const void *
 
 void klist_reverse(klist *list)
 {
-    knode *head = list->head;
-    knode *tail = list->tail;
+    struct node *head = list->head;
+    struct node *tail = list->tail;
 
     list->head = tail;
     list->tail = head;
     while (head != NULL)
     {
-        knode *temp;
+        struct node *temp;
 
         temp = head->prev;
         head->prev = head->next;
@@ -442,11 +446,11 @@ void klist_clear(klist *list, void (*func)(void *))
 {
     if (func != NULL)
     {
-        knode *node = list->head;
+        struct node *node = list->head;
 
         while (node != NULL)
         {
-            knode *next = node->next;
+            struct node *next = node->next;
 
             func(klist_data(list, node));
             node = next;
