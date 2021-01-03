98b1cfa40e3acdd23bad9ac5fde34c4dc0a4e548
David Ranieri <50286622+davranfor@users.noreply.github.com>
2021-01-03 17:41:41 +0100

    Add files via upload

---
+++ vector/main.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "vector.h"
+
+struct data
+{
+    int key;
+    char *value;
+};
+
+static char *keytostr(int key)
+{
+    char buf[32];
+    size_t len;
+
+    len = (size_t)snprintf(buf, sizeof buf, "(%d)", key);
+
+    char *str = malloc(len + 1);
+
+    if (str == NULL)
+    {
+        perror("keytostr");
+        exit(EXIT_FAILURE);
+    }
+    memcpy(str, buf, len + 1);
+    return str;
+}
+
+static int comp(const void *pa, const void *pb)
+{
+    const struct data *a = pa;
+    const struct data *b = pb;
+
+    return a->key < b->key ? -1 : a->key > b->key;
+}
+
+static void delete(void *data)
+{
+    free(((struct data *)data)->value);
+}
+
+static void print(const vector *table)
+{
+    const struct data *item = table->data;
+
+    for (size_t i = 0; i < table->size; i++)
+    {
+        printf("%d %s\n", item[i].key, item[i].value);
+    }
+}
+
+static vector *data;
+
+static void clean(void)
+{
+    vector_destroy(data);
+}
+
+int main(void)
+{
+    atexit(clean);
+    srand((unsigned)time(NULL));
+
+    data = vector_create(sizeof(struct data), delete);
+    if (data == NULL)
+    {
+        perror("vector_create");
+        exit(EXIT_FAILURE);
+    }
+
+    int size = rand() % 10;
+    struct data *item;
+
+    for (int iter = 0; iter < size; iter++)
+    {
+        item = vector_resize(data, +1);
+        if (item == NULL)
+        {
+            perror("vector_resize");
+            exit(EXIT_FAILURE);
+        }
+        item->key = rand() % 10;
+        item->value = keytostr(item->key);
+    }
+    item = (struct data[]){{10, keytostr(10)}, {11, keytostr(11)}};
+    if ((item[0].value == NULL) || (item[1].value == NULL))
+    {
+        perror("keytostr");
+        exit(EXIT_FAILURE);
+    }
+    if (vector_concat(data, item, 2) == NULL)
+    {
+        perror("vector_concat");
+        exit(EXIT_FAILURE);
+    }
+    printf("Inserted: %zu elements\n", data->size);
+    puts("Unsorted:");
+    print(data);
+    vector_sort(data, comp);
+    puts("Sorted:");
+    print(data);
+    item = &((struct data){.key = 5});
+    printf("Searching %d\n", item->key);
+    if ((item = vector_bsearch(data, item, comp)))
+    {
+        printf("%d %s\n", item->key, item->value);
+    }
+    else
+    {
+        puts("Not found");
+    }
+    puts("Deleting last element");
+    vector_resize(data, -1);
+    print(data);
+    return 0;
+}
+
---
+++ vector/vector.c
@@ -0,0 +1,313 @@
+/*! 
+ *  \brief     Vector
+ *  \author    David Ranieri <davranfor@gmail.com>
+ *  \copyright GNU Public License.
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include "vector.h"
+
+#define VECTOR_ITEM(v, i) ((unsigned char *)((v)->data) + (v)->szof * (i))
+
+vector *vector_create(size_t szof, void (*fdel)(void *))
+{
+    vector *vec = calloc(1, sizeof(*vec));
+
+    if (vec != NULL)
+    {
+        vec->szof = szof;
+        vec->fdel = fdel;
+    }
+    return vec;
+}
+
+/* Round up to the next power of 2 */
+static size_t next_size(size_t size)
+{
+    size--;
+    size |= size >> 1;
+    size |= size >> 2;
+    size |= size >> 4;
+    size |= size >> 8;
+    size |= size >> 16;
+    if (sizeof(size) >= 8)
+    {
+        size |= size >> 32;
+    }
+    size++;
+    return size;
+}
+
+static void *resize(vector *vec, size_t size)
+{
+    return realloc(vec->data, vec->szof * size);
+}
+
+static void *increment(vector *vec, size_t size)
+{
+    size_t room = next_size(vec->size);
+    void *data;
+
+    if (vec->size + size > room)
+    {
+        room = next_size(vec->size + size);
+        data = resize(vec, room);
+        if (data == NULL)
+        {
+            return NULL;
+        }
+        vec->data = data;
+    }
+    data = VECTOR_ITEM(vec, vec->size);
+    vec->size += size;
+    return data;
+}
+
+static void *decrement(vector *vec, size_t size)
+{
+    if (vec->size == 0)
+    {
+        return NULL;
+    }
+
+    size_t room = next_size(vec->size);
+
+    if (size > vec->size)
+    {
+        size = vec->size;
+    }
+    if (vec->fdel != NULL)
+    {
+        while (size--)
+        {
+            vec->fdel(VECTOR_ITEM(vec, --vec->size));
+        }
+    }
+    else
+    {
+        vec->size -= size;
+    }
+    if (vec->size == 0)
+    {
+        free(vec->data);
+        vec->data = NULL;
+        return NULL;
+    }
+    if (vec->size <= room / 2)
+    {
+        room = next_size(vec->size);
+
+        void *data = resize(vec, room);
+
+        /*
+         * Since the API returns `NULL` when the vector is empty (0 items),
+         * we don't return NULL in the very unlikely case that realloc fail
+         * allocating less memory
+         */
+        if (data != NULL)
+        {
+            vec->data = data;
+        }
+    }
+    return VECTOR_ITEM(vec, vec->size);
+}
+
+void *vector_resize(vector *vec, int size)
+{
+    if (size > 0)
+    {
+        return increment(vec, (size_t)+size);
+    }
+    if (size < 0)
+    {
+        return decrement(vec, (size_t)-size);
+    }
+    return NULL;
+}
+
+void *vector_insert(vector *vec, size_t index)
+{
+    if (index > vec->size)
+    {
+        return NULL;
+    }
+    if (index == vec->size)
+    {
+        return increment(vec, 1);
+    }
+
+    size_t room = next_size(vec->size);
+
+    if (vec->size + 1 > room)
+    {
+        room = next_size(vec->size + 1);
+
+        void *data = resize(vec, room);
+
+        if (data == NULL)
+        {
+            return NULL;
+        }
+        vec->data = data;
+    }
+    memmove(
+        VECTOR_ITEM(vec, index + 1),
+        VECTOR_ITEM(vec, index),
+        vec->szof * (vec->size++ - index)
+    );
+    return VECTOR_ITEM(vec, index);
+}
+
+void *vector_delete(vector *vec, size_t index)
+{
+    if (index >= vec->size)
+    {
+        return NULL;
+    }
+    if (index == vec->size - 1)
+    {
+        return decrement(vec, 1);
+    }
+
+    size_t room = next_size(vec->size--);
+
+    if (vec->fdel != NULL)
+    {
+        vec->fdel(VECTOR_ITEM(vec, index));
+    }
+    memmove(
+        VECTOR_ITEM(vec, index),
+        VECTOR_ITEM(vec, index + 1),
+        vec->szof * (vec->size - index)
+    );
+    if (vec->size <= room / 2)
+    {
+        room = next_size(vec->size);
+
+        void *data = resize(vec, room);
+
+        if (data != NULL)
+        {
+            vec->data = data;
+        }
+    }
+    return VECTOR_ITEM(vec, index);
+}
+
+void *vector_copy(vector *vec, const void *source, size_t size)
+{
+    if (size == 0)
+    {
+        return NULL;
+    }
+
+    size_t diff = (size > vec->size) ? size - vec->size : 0;
+    size_t room = next_size(vec->size);
+
+    if (vec->fdel != NULL)
+    {
+        for (size_t item = 0; (item < size) && (item < vec->size); item++)
+        {
+            vec->fdel(VECTOR_ITEM(vec, item));
+        }
+    }
+    if ((diff > 0) && (vec->size + diff > room))
+    {
+        room = next_size(vec->size + diff);
+
+        void *data = resize(vec, room);
+
+        if (data == NULL)
+        {
+            return NULL;
+        }
+        vec->data = data;
+    }
+    memcpy(vec->data, source, vec->szof * size);
+    vec->size += diff;
+    return vec->data;
+}
+
+void *vector_concat(vector *vec, const void *source, size_t size)
+{
+    if (size == 0)
+    {
+        return NULL;
+    }
+
+    size_t room = next_size(vec->size);
+    void *data;
+
+    if (vec->size + size > room)
+    {
+        room = next_size(vec->size + size);
+        data = resize(vec, room);
+        if (data == NULL)
+        {
+            return NULL;
+        }
+        vec->data = data;
+    }
+    data = memcpy(VECTOR_ITEM(vec, vec->size), source, vec->szof * size);
+    vec->size += size;
+    return data;
+}
+
+void vector_sort(vector *vec, int (*comp)(const void *, const void *))
+{
+    qsort(vec->data, vec->size, vec->szof, comp);
+}
+
+/* Binary search */
+void *vector_bsearch(const vector *vec, const void *key, int (*comp)(const void *, const void *))
+{
+    return bsearch(key, vec->data, vec->size, vec->szof, comp);
+}
+
+/* Silence compiler casting non const to const with `(void *)const_var` */
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wcast-qual"
+
+/* Linear search */
+void *vector_lsearch(const vector *vec, const void *key, int (*comp)(const void *, const void *))
+{
+    const unsigned char *item = vec->data;
+    const unsigned char *last = item + vec->szof * vec->size;
+
+    while (item < last)
+    {
+        if (comp(item, key) == 0)
+        {
+            return (void *)item;
+        }
+        item += vec->szof;
+    }
+    return NULL;
+}
+
+#pragma GCC diagnostic pop
+
+vector *vector_clear(vector *vec)
+{
+    if (vec->fdel != NULL)
+    {
+        for (size_t item = 0; item < vec->size; item++)
+        {
+            vec->fdel(VECTOR_ITEM(vec, item));
+        }
+    }
+    free(vec->data);
+    vec->data = NULL;
+    vec->size = 0;
+    return vec;
+}
+
+void vector_destroy(vector *vec)
+{
+    if (vec != NULL)
+    {
+        free(vector_clear(vec));
+    }
+}
+
