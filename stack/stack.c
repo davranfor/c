82e97c2faf07ab4f81720521a575f807f282d9c5
David Ranieri <50286622+davranfor@users.noreply.github.com>
2021-08-05 23:57:18 +0200

    Add files via upload
    Fix - Dangling pointer

--- stack/stack.c
+++ stack/stack.c
@@ -100,20 +100,23 @@ size_t stack_size(const stack *list)
 
 void stack_destroy(stack *list, void (*func)(void *))
 {
-    struct node *node = list->head;
-
-    while (node != NULL)
+    if (list != NULL)
     {
-        if (func != NULL)
+        struct node *node = list->head;
+
+        while (node != NULL)
         {
-            func(node->data);
+            if (func != NULL)
+            {
+                func(node->data);
+            }
+
+            struct node *temp = node->next;
+
+            free(node);
+            node = temp;
         }
-
-        struct node *temp = node->next;
-
-        free(node);
-        node = temp;
+        free(list);
     }
-    free(list);
 }
 
