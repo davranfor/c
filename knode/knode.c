142309d2b598fd8beef8315460d8e4e60976e4c3
David Ranieri <50286622+davranfor@users.noreply.github.com>
2021-10-02 10:01:10 +0200

    Add files via upload

--- knode/knode.c
+++ knode/knode.c
@@ -16,15 +16,13 @@ struct knode
     size_t size;
 };
 
-typedef uintptr_t addr;
-
 knode *knode_create(size_t size)
 {
     knode *list = calloc(1, sizeof *list);
 
     if (list != NULL)
     {
-        size_t align = alignof(addr);
+        size_t align = alignof(uintptr_t);
 
         // Round size up to nearest multiple of alignof(uintptr_t)
         list->szof = (size + (align - 1)) / align * align;
@@ -32,12 +30,11 @@ knode *knode_create(size_t size)
     return list;
 }
 
-#define knode_const_addr(list, data) ((const uintptr_t *)((uintptr_t)data + list->szof))
 #define knode_addr(list, data) ((uintptr_t *)((uintptr_t)data + list->szof))
 
 void *knode_push_head(knode *list)
 {
-    void *data = calloc(1, list->szof + sizeof(addr));
+    void *data = calloc(1, list->szof + sizeof(uintptr_t));
 
     if (data == NULL)
     {
@@ -45,8 +42,8 @@ void *knode_push_head(knode *list)
     }
     if (list->head != NULL)
     {
-        *knode_addr(list, list->head) ^= (addr)data;
-        *knode_addr(list, data) = (addr)list->head;
+        *knode_addr(list, list->head) ^= (uintptr_t)data;
+        *knode_addr(list, data) = (uintptr_t)list->head;
     }
     else
     {
@@ -59,7 +56,7 @@ void *knode_push_head(knode *list)
 
 void *knode_push_tail(knode *list)
 {
-    void *data = calloc(1, list->szof + sizeof(addr));
+    void *data = calloc(1, list->szof + sizeof(uintptr_t));
 
     if (data == NULL)
     {
@@ -67,8 +64,8 @@ void *knode_push_tail(knode *list)
     }
     if (list->tail != NULL)
     {
-        *knode_addr(list, list->tail) ^= (addr)data;
-        *knode_addr(list, data) = (addr)list->tail;
+        *knode_addr(list, list->tail) ^= (uintptr_t)data;
+        *knode_addr(list, data) = (uintptr_t)list->tail;
     }
     else
     {
@@ -88,12 +85,12 @@ void *knode_pop_head(knode *list)
         return NULL;
     }
 
-    addr *head = knode_addr(list, data);
+    uintptr_t *head = knode_addr(list, data);
     void *next = (void *)*head;
 
     if (next != NULL)
     {
-        *knode_addr(list, next) ^= (addr)data;
+        *knode_addr(list, next) ^= (uintptr_t)data;
         *head = 0;
     }
     else
@@ -114,12 +111,12 @@ void *knode_pop_tail(knode *list)
         return NULL;
     }
 
-    addr *tail = knode_addr(list, data);
+    uintptr_t *tail = knode_addr(list, data);
     void *prev = (void *)*tail;
 
     if (prev != NULL)
     {
-        *knode_addr(list, prev) ^= (addr)data;
+        *knode_addr(list, prev) ^= (uintptr_t)data;
         *tail = 0;
     }
     else
@@ -154,7 +151,7 @@ void *knode_fetch(const knode *list, kiter *iter, const void *data)
         return list->tail;
     }
     
-    addr next = *knode_const_addr(list, data) ^ *iter;
+    uintptr_t next = *knode_addr(list, data) ^ *iter;
 
     *iter = (kiter)data;
     return (void *)next;
@@ -182,7 +179,7 @@ void *knode_index(const knode *list, size_t index)
     {
         void *temp = node;
 
-        node = (void *)(*knode_addr(list, node) ^ (addr)prev);
+        node = (void *)(*knode_addr(list, node) ^ (uintptr_t)prev);
         prev = temp;
     }
     return node;
@@ -199,12 +196,12 @@ void knode_clear(knode *list, void (*func)(void *))
 
     while (node != NULL)
     {
-        addr *head = knode_addr(list, node);
+        uintptr_t *head = knode_addr(list, node);
         void *next = (void *)*head;
 
         if (next != NULL)
         {
-            *knode_addr(list, next) ^= (addr)node;
+            *knode_addr(list, next) ^= (uintptr_t)node;
             *head = 0;
         }
         if (func != NULL)
