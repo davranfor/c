64a78b7457d93c59baa90dd73ebe272a35d088bf
David Ranieri <50286622+davranfor@users.noreply.github.com>
2021-11-16 20:58:42 +0100

    Add files via upload

--- json/main.c
+++ json/main.c
@@ -1,31 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
-#include "utils.h"
 #include "json.h"
 
-static json *parse(const char *path)
+static char *read_file(const char *path)
 {
-    char *text = file_read(path);
+    FILE *file = fopen(path, "rb");
+
+    if (file == NULL)
+    {
+        return NULL;
+    }
+
+    char *str = NULL;
+
+    do
+    {
+        if (fseek(file, 0L, SEEK_END) == -1)
+        {
+            break;
+        }
+
+        long tell = ftell(file);
+
+        if ((tell == -1) || (fseek(file, 0L, SEEK_SET) == -1))
+        {
+            break;
+        }
+
+        size_t size = (size_t)tell;
+
+        str = malloc(size + 1);
+        if (str != NULL)
+        {
+            if (fread(str, 1, size, file) == size)
+            {
+                str[size] = '\0';
+            }
+            else
+            {
+                free(str);
+            }
+        }
+    }
+    while (0);
+    fclose(file);
+    return str;
+}
+
+static json *read_json(const char *path)
+{
+    char *text = read_file(path);
 
     if (text == NULL)
     {
-        fprintf(stderr, "Can't read %s\n", path);
+        perror("read_json");
         exit(EXIT_FAILURE);
     }
 
     json *node = json_parse(text);
 
-    if (node == NULL)
-    {
-        fprintf(stderr, "Can't parse %s\n", path);
-        free(text);
-        exit(EXIT_FAILURE);
-    }
-    else
-    {
-        free(text);
-    }
+    free(text);
     return node;
 }
 
@@ -33,8 +68,14 @@ int main(void)
 {
     setlocale(LC_CTYPE, "");
 
-    json *node = parse("test.json");
+    const char *path = "test.json";
+    json *node = read_json(path);
 
+    if (node == NULL)
+    {
+        perror("read_json");
+        exit(EXIT_FAILURE);
+    }
     json_print(node);
     json_free(node);
     return 0;
