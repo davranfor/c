40ef348cee038a802e99fd26115677509edbbbf6
David Ranieri <50286622+davranfor@users.noreply.github.com>
2022-10-20 22:53:00 +0200

    Add files via upload

---
+++ json/json_format.h
@@ -0,0 +1,23 @@
+/*!
+ *  \brief     JSON Schema validator
+ *  \author    David Ranieri <davranfor@gmail.com>
+ *  \copyright GNU Public License.
+ */
+
+#ifndef JSON_FORMAT_H
+#define JSON_FORMAT_H
+
+typedef int (*schema_format)(const char *);
+
+int test_is_date(const char *);
+int test_is_time(const char *);
+int test_is_date_time(const char *);
+int test_is_email(const char *);
+int test_is_ipv4(const char *);
+int test_is_ipv6(const char *);
+int test_is_uuid(const char *);
+
+int test_pattern(const char *, const char *);
+
+#endif /* JSON_FORMAT_H */
+
