487351a373b465870d898808aa46da0535f0be6e
David Ranieri <50286622+davranfor@users.noreply.github.com>
2023-04-20 17:56:03 +0200

    Add files via upload

--- json/src/json_pointer.c
+++ json/src/json_pointer.c
@@ -3,49 +3,44 @@
 #include <string.h>
 #include "json_struct.h"
 
-/*
-Evaluation of each reference token begins by decoding any escaped character sequence.
-This is performed by first transforming any occurrence of the sequence '~1' to '/',
-and then transforming any occurrence of the sequence '~0' to '~'.
-By performing the substitutions in this order, an implementation avoids the error of
-turning '~01' first into '~1' and then into '/', which would be incorrect
-(the string '~01' correctly becomes '~1' after transformation).
-*/
-static int compare(const char *a, const char *b, const char *end)
+static int compare(const char *name, const char *path, const char *end)
 {
-    while (b < end)
+    for (; path < end; name++, path++)
     {
-        if (*a == '/')
+        /* Any occurrence of '~' in 'name' must match with '~0' in 'path' */
+        if (*name == '~')
         {
-            if ((b[0] == '~') && (b[1] == '1'))
+            if ((path[0] == '~') && (path[1] == '0'))
             {
-                a += 1;
-                b += 2;
-                continue;
+                path += 1;
             }
+            else
+            {
+                return 0;
+            }
+        }
+        /* Any occurrence of '/' in 'name' must match with '~1' in 'path' */
+        else if (*name == '/')
+        {
+            if ((path[0] == '~') && (path[1] == '1'))
+            {
+                path += 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        /* Doesn't match */
+        else if (*name != *path)
+        {
             return 0;
         }
-        if (*a == '~')
-        {
-            if ((b[0] == '~') && (b[1] == '0'))
-            {
-                a += 1;
-                b += 2;
-                continue;
-            }
-            return 0;
-        }
-        if (*a != *b)
-        {
-            break;
-        }
-        a++;
-        b++;
     }
-    return (*a == '\0') && (b == end);
+    return (*name == '\0') && (path == end);
 }
 
-static json *equal(const json *root, const char *name, const char *end)
+static json *equal(const json *root, const char *path, const char *end)
 {
     if ((root == NULL) || (root->type != JSON_OBJECT))
     {
@@ -53,7 +48,7 @@ static json *equal(const json *root, const char *name, const char *end)
     }
     for (json *node = root->child; node != NULL; node = node->next)
     {
-        if (compare(node->name, name, end))
+        if (compare(node->name, path, end))
         {
             return node;
         }
